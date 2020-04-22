01c276c66771ec17b5806e97f24a5edec9e41191
Ishant Kushwaha <ishant.kushwaha@nagarro.com>
2020-04-22 14:38:27 +0530

    final commit

---
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment1',
+  templateUrl: './assignment1.component.html',
+  styleUrls: ['./assignment1.component.css']
+})
+export class Assignment1Component {
+
+value : string;
+
+onInput(event : string){
+  this.value = event;
+} 
+
+}
+
